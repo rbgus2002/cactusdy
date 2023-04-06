56c4c358ad3edf089fb56091ffd900b7e17d0dfc
choegyuhyeon <rbgus2002@naver.com>
2023-04-06 22:33:33 +0900

    - Add : ALL : init spring project

---
+++ src/main/java/ssu/groupstudy/GroupstudyApplication.java
@@ -0,0 +1,11 @@
+package ssu.groupstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupstudyApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GroupstudyApplication.class, args);
+	}
+}
