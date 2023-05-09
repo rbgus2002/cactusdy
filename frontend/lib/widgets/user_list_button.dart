787c7113c5fc162bb7df459db9fd82e9c24f7f51
rbgus2002 <rbgus2002@naver.com>
2023-05-09 23:16:24 +0900

    [F] Add : user_list_button : 이미지 별 margin 추가

--- frontend/lib/widgets/user_list_button.dart
+++ frontend/lib/widgets/user_list_button.dart
@@ -3,30 +3,31 @@ import 'package:flutter/cupertino.dart';
 import '../models/user.dart';
 import 'circle_button.dart';
 
-class UserList extends StatelessWidget {
+class UserListButton extends StatelessWidget {
   final List<User> userList;
   final onTap;
+  final double scale;
 
-
-  const UserList({
+  const UserListButton({
     Key? key,
     required this.userList,
     this.onTap,
+    required this.scale,
   }) : super(key: key);
-
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
-      padding: const EdgeInsets.all(5),
       child: Row(
         children: [
-          for(User user in userList)
-            CircleButton(scale: 45.0, child: null, onTap: onTap), // TODO : image null 처리 방법 고안
+          for (User user in userList)
+            Container(
+              margin: const EdgeInsets.fromLTRB(1, 0, 3, 1),
+              child: CircleButton(scale: scale, child: null, onTap: onTap),
+            )
         ],
       ),
     );
   }
-
 }
