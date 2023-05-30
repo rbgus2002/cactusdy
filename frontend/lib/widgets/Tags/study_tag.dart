39fc79d88740c00ebd1fe46f3bc9d542afc66ff5
Arkady <Arkady1356@naver.com>
2023-05-31 02:27:27 +0900

    [F] Change : Tag : Tag Class 분리 및 디렉토리명 수정

---
+++ frontend/lib/widgets/Tags/study_tag.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:group_study_app/themes/design.dart';
+import 'package:group_study_app/themes/text_styles.dart';
+import 'package:group_study_app/widgets/Tags/tag.dart';
+import 'package:group_study_app/widgets/buttons/circle_button.dart';
+
+class StudyTag extends TagButton {
+  static const studyTagPadding = EdgeInsets.fromLTRB(3, 3, 10, 3);
+  Image? image;
+  String name;
+
+  StudyTag({
+    this.image,
+    this.name = "",
+    super.key,
+    super.color,
+    super.onTap }) : super(
+      padding: studyTagPadding,
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+        children: [
+          const CircleButton(scale:20, onTap: null),
+          Design.padding5,
+          Text(name, style: TextStyles.tagTextStyle, textAlign: TextAlign.center),
+        ],)
+  );
+}
--- frontend/lib/widgets/Tags/tag.dart
+++ frontend/lib/widgets/Tags/tag.dart
@@ -3,7 +3,7 @@ import 'package:flutter/rendering.dart';
 import 'package:group_study_app/themes/color_styles.dart';
 import 'package:group_study_app/themes/design.dart';
 import 'package:group_study_app/themes/text_styles.dart';
-import 'package:group_study_app/widgets/circle_button.dart';
+import 'package:group_study_app/widgets/buttons/circle_button.dart';
 
 // 수정할 것 list
 // user_line_profile : profile 수정 버튼 onPressed
@@ -54,43 +54,4 @@ class TagButton extends StatelessWidget{
     );
 
   }
-}
-
-class UserStateTag extends TagButton {
-  static const userStateTagPadding = EdgeInsets.fromLTRB(10, 0, 10, 0);
-  String text;
-
-  UserStateTag({
-    this.text = "",
-    super.key,
-    super.color,
-    super.onTap }) : super(
-      padding: userStateTagPadding,
-      child: Text(text, textWidthBasis: TextWidthBasis.parent,
-        style: TextStyles.tagTextStyle, textAlign: TextAlign.center,),
-  );
-}
-
-class StudyTag extends TagButton {
-  static const studyTagPadding = EdgeInsets.fromLTRB(3, 3, 10, 3);
-  Image? image;
-  String name;
-
-  StudyTag({
-    this.image,
-    this.name = "",
-    super.key,
-    super.color,
-    super.onTap }) : super(
-      padding: studyTagPadding,
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-        children: [
-          const CircleButton(scale:20, onTap: null),
-          Design.padding5,
-          Text(name, style: TextStyles.tagTextStyle, textAlign: TextAlign.center),
-        ],)
-   );
 }
