7c18c26294d7389644d1a387a5f273f5c96d6afa
Arkady <Arkady1356@naver.com>
2023-05-31 02:28:08 +0900

    [F] Change : Task : Task Class 분리 및 디렉토리명 수정

--- frontend/lib/widgets/tasks/check_box_task.dart
+++ frontend/lib/widgets/tasks/check_box_task.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:group_study_app/themes/text_styles.dart';
 import 'package:group_study_app/utilities/test.dart';
-import 'package:group_study_app/widgets/Task/task.dart';
+import 'package:group_study_app/widgets/tasks/task.dart';
 
 class CheckBoxTask extends Task {
   bool _isChecked = false;
