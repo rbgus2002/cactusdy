92a55107bf82c13ab1b1479c175bede7e6719668
rbgus2002 <rbgus2002@naver.com>
2023-06-03 11:56:06 +0900

    [B] Change : Study : Participant 관련 test code 분리

--- backend/src/test/java/ssu/groupstudy/domain/study/repository/ParticipantRepositoryTest.java
+++ backend/src/test/java/ssu/groupstudy/domain/study/repository/ParticipantRepositoryTest.java
@@ -1,71 +1,125 @@
 package ssu.groupstudy.domain.study.repository;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import ssu.groupstudy.domain.common.RepositoryTest;
 import ssu.groupstudy.domain.study.domain.Participants;
 import ssu.groupstudy.domain.study.domain.Study;
 import ssu.groupstudy.domain.study.domain.Participant;
 import ssu.groupstudy.domain.study.dto.reuqest.CreateStudyRequest;
+import ssu.groupstudy.domain.study.exception.CanNotLeaveStudyException;
+import ssu.groupstudy.domain.study.exception.InviteAlreadyExistsException;
 import ssu.groupstudy.domain.user.domain.User;
 import ssu.groupstudy.domain.user.dto.request.SignUpRequest;
+import ssu.groupstudy.domain.user.exception.UserNotParticipatedException;
 import ssu.groupstudy.domain.user.repository.UserRepository;
+import ssu.groupstudy.global.ResultCode;
 
 import java.util.Set;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-class ParticipantRepositoryTest {
-    @Autowired
-    ParticipantRepository participantRepository;
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private StudyRepository studyRepository;
-
-    private User getUser() {
-        return SignUpRequest.builder()
-                .name("최규현")
-                .email("rbgus200@@naver.com")
-                .nickName("규규")
-                .phoneModel("")
-                .picture("")
-                .build().toEntity();
-    }
-
-    private Study getStudy(User hostUser) {
-        return CreateStudyRequest.builder()
-                .studyName("AlgorithmSSU")
-                .hostUserId(hostUser.getUserId())
-                .detail("PS")
-                .picture("")
-                .build().toEntity(hostUser);
-    }
-
-    @DisplayName("사용자를 스터디에 초대한다.")
+class ParticipantRepositoryTest extends RepositoryTest {
+    @DisplayName("스터디에 소속되어있는 사용자인지 확인한다")
     @Test
-    void 성공_스터디초대() {
-        //given
-        final User hostUser = userRepository.save(getUser());
-        final Study savedStudy = studyRepository.save(getStudy(hostUser));
+    void isParticipated() {
+        // given
+        userRepository.save(최규현);
+        studyRepository.save(알고리즘스터디);
 
-        final User newUser = userRepository.save(SignUpRequest.builder()
-                .name("장재우")
-                .email("arkady@@naver.com")
-                .nickName("킹적화")
-                .build().toEntity());
+        // when
+        boolean isParticipated = 알고리즘스터디.isParticipated(최규현);
 
-        //when
-        savedStudy.invite(newUser);
+        // then
+        assertThat(isParticipated).isTrue();
+    }
 
-        //then
-        assertThat(savedStudy.getParticipants().getHostUser().getName()).isEqualTo("최규현");
-        assertThat(savedStudy.getParticipants().getParticipants().size()).isEqualTo(2);
+    @Nested
+    class invite {
+        @DisplayName("이미 참여중인 사용자를 스터디에 초대하면 예외를 던진다")
+        @Test
+        void fail_alreadyExistUser() {
+            // given
+            userRepository.save(최규현);
+            userRepository.save(장재우);
+            studyRepository.save(알고리즘스터디);
+
+            // when, then
+            assertThatThrownBy(() -> 알고리즘스터디.invite(최규현))
+                    .isInstanceOf(InviteAlreadyExistsException.class)
+                    .hasMessage(ResultCode.DUPLICATE_INVITE_USER.getMessage());
+        }
+
+        @DisplayName("새로운 사용자를 스터디에 초대한다")
+        @Test
+        void success() {
+            // given
+            Long userId = userRepository.save(최규현).getUserId();
+            userRepository.save(장재우);
+            studyRepository.save(알고리즘스터디);
+
+            // when
+            알고리즘스터디.invite(장재우);
+            Participant participant = participantRepository.findParticipantByUserAndStudy(장재우, 알고리즘스터디).get();
+
+            // then
+            assertAll(
+                    () -> assertThat(알고리즘스터디.getParticipants().getParticipants().size()).isEqualTo(2),
+                    () -> assertThat(알고리즘스터디.isParticipated(장재우)),
+                    () -> assertThat(participant.getUser()).isEqualTo(장재우)
+            );
+        }
+    }
+
+    @Nested
+    class leave {
+        @DisplayName("참여중이지 않은 사용자가 스터디 탈퇴를 시도하면 예외를 던진다")
+        @Test
+        void fail_userNotFound() {
+            // given
+            userRepository.save(최규현);
+            userRepository.save(장재우);
+            studyRepository.save(알고리즘스터디);
+
+            // when, then
+            assertThatThrownBy(() -> 알고리즘스터디.leave(장재우))
+                    .isInstanceOf(UserNotParticipatedException.class)
+                    .hasMessage(ResultCode.USER_NOT_PARTICIPATED.getMessage());
+        }
+
+        @DisplayName("방장은 스터디에 탈퇴할 수 없다")
+        @Test
+        void fail_hostUserInvalid() {
+            // given
+            userRepository.save(최규현);
+            studyRepository.save(알고리즘스터디);
+
+            // when, then
+            assertThatThrownBy(() -> 알고리즘스터디.leave(최규현))
+                    .isInstanceOf(CanNotLeaveStudyException.class)
+                    .hasMessage(ResultCode.HOST_USER_CAN_NOT_LEAVE_STUDY.getMessage());
+        }
+
+        @DisplayName("사용자가 스터디에서 탈퇴한다")
+        @Test
+        void success() {
+            // given
+            userRepository.save(최규현).getUserId();
+            userRepository.save(장재우);
+            studyRepository.save(알고리즘스터디);
+
+            // when
+            알고리즘스터디.invite(장재우);
+            알고리즘스터디.leave(장재우);
+
+            // then
+            assertThat(알고리즘스터디.getParticipants().getParticipants().size()).isEqualTo(1);
+        }
     }
 }
