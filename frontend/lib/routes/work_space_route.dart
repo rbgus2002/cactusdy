5d9a4a725ef30129489c17a9c88e16d08143feb9
Arkady <Arkady1356@naver.com>
2023-06-06 03:24:09 +0900

    [F] Add : Home : Home route 추가 및 StudyGroupPanel 수정

--- frontend/lib/routes/work_space_route.dart
+++ frontend/lib/routes/work_space_route.dart
@@ -7,7 +7,10 @@ import 'package:group_study_app/widgets/Tags/user_state_tag.dart';
 import 'package:group_study_app/widgets/buttons/percent_circle_button.dart';
 import 'package:group_study_app/widgets/charts/bar_chart.dart';
 import 'package:group_study_app/widgets/charts/chart.dart';
+import 'package:group_study_app/widgets/dialogs/user_profile_dialog.dart';
 import 'package:group_study_app/widgets/panels/round_info_panel.dart';
+import 'package:group_study_app/widgets/round_info.dart';
+import 'package:group_study_app/widgets/tags/study_group_tag.dart';
 import 'package:group_study_app/widgets/user_line_profile.dart';
 
 class WorkSpaceRoute extends StatefulWidget {
@@ -28,16 +31,30 @@ class _WorkSpaceRoute extends State<StatefulWidget> {
             padding: EdgeInsets.all(20),
             child: Column(
               children: [
-                PercentCircleButton(scale: 100.0, image: null, percentInfos: [ PercentInfo(percent: 0.6, color: ColorStyles.red)], onTap: Test.onTabTest),
+                PercentCircleButton(scale: 60.0, image: null, percentInfos: [ PercentInfo(percent: 0.6, color: ColorStyles.red)], onTap: () { UserProfileDialog.showProfileDialog(context); }),
                 const UserLineProfile(scale: 50.0, image: null, onTap: Test.onTabTest, nickName: "NickName", comment: "Comment!",),
                 const SizedBox(
                   height: 5,
                 ),
-                RoundInfoPanel(roundIdx: 3, place: "숭실대학교 정보과학관", date: DateTime.now(), userList: List<User>.generate(30, (index) => User(index, "d", "d"))),
+                //RoundInfoPanel(roundIdx: 3, place: "숭실대학교 정보과학관", date: DateTime.now(), userList: List<User>.generate(30, (index) => User(index, "d", "d"))),
+                //RoundInfo(roundIdx: 3, tag: "TAG"),
+
                 UserStateTag(text: "USE", color: Colors.red,),
                 BarChart(percentInfos: [ PercentInfo(percent: 0.4, color: ColorStyles.red)
-                , PercentInfo(percent: 0.1, color: ColorStyles.orange), PercentInfo(percent: 0.2, color: ColorStyles.green) ],
-            stroke: 30,),
+                , PercentInfo(percent: 0.1, color: ColorStyles.orange), PercentInfo(percent: 0.2, color: ColorStyles.green) ], stroke: 30,),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      StudyGroupTag(color: ColorStyles.red, name: "Algorithm",),
+                      StudyGroupTag(color: ColorStyles.orange, name: "TOEIC"),
+
+                      StudyGroupTag(color: ColorStyles.orange, name: "TOEIC"),
+                      StudyGroupTag(color: ColorStyles.orange, name: "TOEIC"),
+
+                    ],
+                  ),
+                )
               ],
             )
           )
