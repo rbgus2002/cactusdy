7b229c23782b7e3c3fbbb88ffa143836b722898e
Arkady <Arkady1356@naver.com>
2023-07-15 13:04:39 +0900

    [F] change : Utility : 글자 크기 및 기타 유틸리티 클래스 추가

--- frontend/lib/utilities/test.dart
+++ frontend/lib/utilities/test.dart
@@ -3,9 +3,9 @@ import 'package:group_study_app/models/study.dart';
 import 'package:group_study_app/models/user.dart';
 
 class Test {
-  static final User testUser = User(userId: 1, nickName: "Arkady",statusMessage: "", picture: "");
+  static final User testUser = User(userId: 1, nickname: "Arkady",statusMessage: "", picture: "");
   static final Study testStudy = Study(studyId: 1);
-  static final List<User> testUserList = List<User>.generate(30, (index) => User(userId: 0, nickName: "d", statusMessage: "", picture: ""));
+  static final List<User> testUserList = List<User>.generate(30, (index) => User(userId: 0, nickname: "d", statusMessage: "", picture: ""));
 
   static void onTabTest() {
     print('Tab!');
---
+++ frontend/lib/utilities/util.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Util {
+  static void pushRoute(BuildContext context, WidgetBuilder builder) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: builder),
+    );
+  }
+
+
+
+}
