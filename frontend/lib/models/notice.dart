8fe4a2467151d68ec2b61eb990c7616a615f1431
Arkady <Arkady1356@naver.com>
2023-07-15 13:05:21 +0900

    [F] change : Models : Comment, Notice, User에 대한 Models 수정

--- frontend/lib/models/notice.dart
+++ frontend/lib/models/notice.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:group_study_app/models/notice_summary.dart';
 import 'package:group_study_app/models/user.dart';
 import 'package:group_study_app/services/database_service.dart';
 import 'package:http/http.dart' as http;
@@ -11,22 +12,53 @@ class Notice {
 
   final int noticeId;
   final String title;
-  final String content;
-
-  final User writer;
-  final DateTime writingTime;
+  final String contents;
+  final int checkNoticeCount;
+  final DateTime createDate;
+  final String writerNickname;
 
   const Notice({
     required this.noticeId,
     required this.title,
-    required this.content,
-    required this.writer,
-    required this.writingTime,
+    required this.contents,
+    required this.checkNoticeCount,
+    required this.createDate,
+    required this.writerNickname,
   });
 
-  static void getNotice(int noticeId) {
+  factory Notice.fromJson(Map<String, dynamic> json, int noticeId) {
+    return Notice(
+      noticeId: noticeId,
+      title: json["title"],
+      contents: json["contents"],
+      checkNoticeCount: json["checkNoticeCount"],
+      createDate: DateTime.parse(json["createDate"]),
+      writerNickname: json["writerNickname"],
+    );
+  }
 
+  static Future<bool> switchCheckNotice(int noticeId, int userId) async {
+    final response = await http.patch(
+      Uri.parse('${DatabaseService.serverUrl}notices/check?noticeId=$noticeId&userId=$userId'),
+    );
 
+    if (response.statusCode != DatabaseService.SUCCESS_CODE) {
+      throw Exception("Fail to switch check notice");
+    } else {
+      String isChecked = json.decode(response.body)['data']["isChecked"];
+      return (isChecked == "Y");
+    }
+  }
+
+  static Future<Notice> getNotice(int noticeId) async {
+      final response = await http.get(Uri.parse('${DatabaseService.serverUrl}notices?noticeId=$noticeId'));
+
+      if (response.statusCode != DatabaseService.SUCCESS_CODE) {
+        throw Exception("Failed to load notice");
+      } else {
+        var responseJson = json.decode(utf8.decode(response.bodyBytes))['data']['noticeInfo'];
+        return Notice.fromJson(responseJson, noticeId);
+      }
   }
 
   static Future<int> createNotice(String title, String contents, int userId, int studyId) async {
