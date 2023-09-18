6fd23a43faccda7231d03776e03acc4d626a0d17
rbgus2002 <rbgus2002@naver.com>
2023-09-18 18:14:30 +0900

    [B] feat : test : 여러 개 annotation 하나로 묶는 custom annotation 생성 #27

---
+++ backend/src/test/java/ssu/groupstudy/domain/common/CustomRepositoryTest.java
@@ -0,0 +1,23 @@
+package ssu.groupstudy.domain.common;
+
+import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@ActiveProfiles("test")
+@DataJpaTest
+@TestPropertySource(locations = "classpath:application-test.yml")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ExtendWith(SoftAssertionsExtension.class)
+public @interface CustomRepositoryTest {
+}
