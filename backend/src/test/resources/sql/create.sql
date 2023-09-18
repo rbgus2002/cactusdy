ce9f3db1f07979308795eb9e04baaa9d9c689826
rbgus2002 <rbgus2002@naver.com>
2023-09-18 16:54:17 +0900

    [B] refactor : TEST : sql script 작성 #27

---
+++ backend/src/test/resources/sql/create.sql
@@ -0,0 +1,151 @@
+create table if not exists `user`
+(
+    user_id        bigint auto_increment primary key,
+    create_date    datetime(6)  not null,
+    modified_date  datetime(6)  not null,
+    activate_date  datetime(6)  not null,
+    delete_yn      char         not null,
+    email          varchar(255) not null,
+    name           varchar(255) not null,
+    nickname       varchar(255) not null,
+    phone_model    varchar(255) null,
+    picture        varchar(255) null,
+    status_message varchar(255) null
+);
+
+create table if not exists study
+(
+    study_id       bigint auto_increment
+        primary key,
+    create_date    datetime(6)  not null,
+    modified_date  datetime(6)  not null,
+    delete_yn      char         not null,
+    detail         varchar(40)  null,
+    invite_qr_code varchar(255) not null,
+    invite_link    varchar(255) not null,
+    picture        varchar(255) null,
+    study_name     varchar(30)  not null,
+    user_id        bigint       not null,
+    constraint FKkhcyngf15w120k4wx6pp21ijn
+        foreign key (user_id) references `user` (user_id)
+);
+
+create table if not exists notice
+(
+    notice_id     bigint auto_increment
+        primary key,
+    create_date   datetime(6)  not null,
+    modified_date datetime(6)  not null,
+    contents      varchar(100) not null,
+    delete_yn     char         not null,
+    pin_yn        char         not null,
+    title         varchar(50)  not null,
+    study_id      bigint       not null,
+    user_id       bigint       not null,
+    constraint FKcvf4mh5se36inrxn7xlh2brfv
+        foreign key (user_id) references `user` (user_id),
+    constraint FKdyaf5r9t0nrj8brk9v61odjc2
+        foreign key (study_id) references study (study_id)
+);
+
+create table if not exists check_notice
+(
+    id        bigint auto_increment
+        primary key,
+    notice_id bigint not null,
+    user_id   bigint not null,
+    constraint FKajbojoc8jmrv3j4xcmrpv688l
+        foreign key (user_id) references `user` (user_id),
+    constraint FKcnhxuf35g6dbj0s9tihgulfd
+        foreign key (notice_id) references notice (notice_id)
+);
+
+create table if not exists comment
+(
+    comment_id        bigint auto_increment
+        primary key,
+    create_date       datetime(6)  not null,
+    modified_date     datetime(6)  not null,
+    contents          varchar(100) not null,
+    delete_yn         char         not null,
+    notice_id         bigint       null,
+    parent_comment_id bigint       null,
+    user_id           bigint       not null,
+    constraint FK8kcum44fvpupyw6f5baccx25c
+        foreign key (user_id) references `user` (user_id),
+    constraint FKhvh0e2ybgg16bpu229a5teje7
+        foreign key (parent_comment_id) references comment (comment_id),
+    constraint FKq7rr5epaoagevts8cop65o31h
+        foreign key (notice_id) references notice (notice_id)
+);
+
+create table if not exists rel_user_study
+(
+    user_study_id bigint auto_increment
+        primary key,
+    create_date   datetime(6)  not null,
+    modified_date datetime(6)  not null,
+    banish_yn     char         not null,
+    color         varchar(127) not null,
+    study_id      bigint       not null,
+    user_id       bigint       not null,
+    id            bigint       not null,
+    constraint FK7erm3ctgn7n8tfvst4auyd5fl
+        foreign key (user_id) references `user` (user_id),
+    constraint FKc74bwwaegphmtuefmi8sfhowv
+        foreign key (study_id) references study (study_id)
+);
+
+create table if not exists round
+(
+    round_id      bigint auto_increment
+        primary key,
+    create_date   datetime(6)  not null,
+    modified_date datetime(6)  not null,
+    study_place   varchar(30)  null,
+    study_time    datetime(6)  null,
+    delete_yn     char         not null,
+    detail        varchar(100) null,
+    study_id      bigint       not null,
+    constraint FK41ah5maxtjcdgiohmdr2s9fai
+        foreign key (study_id) references study (study_id)
+);
+
+create table if not exists rel_user_round
+(
+    user_round_id bigint auto_increment
+        primary key,
+    status_tag    varchar(20) not null,
+    round_id      bigint      not null,
+    user_id       bigint      not null,
+    constraint FKfa12r32ylmnl47houwtwek9ij
+        foreign key (user_id) references `user` (user_id),
+    constraint FKssy2n9lbn5sxtxugavyqkvvog
+        foreign key (round_id) references round (round_id)
+);
+
+create table if not exists rule
+(
+    rule_id       bigint auto_increment
+        primary key,
+    create_date   datetime(6) not null,
+    modified_date datetime(6) not null,
+    delete_yn     char        not null,
+    detail        varchar(50) not null,
+    study_id      bigint      not null,
+    constraint FK8lmh7l1b5g26mr79ie80dv1mv
+        foreign key (study_id) references study (study_id)
+);
+
+create table if not exists task
+(
+    task_id       bigint auto_increment
+        primary key,
+    detail        varchar(200) not null,
+    done_yn       char         not null,
+    task_type     varchar(20)  not null,
+    user_round_id bigint       not null,
+    constraint FK8vl3vhkgegfpqrgle2wb3sp3t
+        foreign key (user_round_id) references rel_user_round (user_round_id)
+);
+
---
+++ backend/src/test/resources/sql/insert.sql
@@ -0,0 +1,25 @@
+-- user
+INSERT INTO `user` (user_id, create_date, modified_date, activate_date, delete_yn, email, name, nickname, phone_model, picture, status_message) VALUES (1, '2023-09-04 21:05:34.061086', '2023-09-04 21:05:34.061086', '2023-09-04 21:05:34.021249', 'N', 'rbgus2002@naver.com', '최규현', '규규', 'string', 'string', '');
+INSERT INTO `user` (user_id, create_date, modified_date, activate_date, delete_yn, email, name, nickname, phone_model, picture, status_message) VALUES (2, '2023-09-04 21:05:51.310402', '2023-09-04 21:05:51.310402', '2023-09-04 21:05:51.309612', 'N', 'arkady@naver.com', '장재우', 'arkady', 'string', 'string', '');
+INSERT INTO `user` (user_id, create_date, modified_date, activate_date, delete_yn, email, name, nickname, phone_model, picture, status_message) VALUES (3, '2023-09-04 21:05:51.310402', '2023-09-04 21:05:51.310402', '2023-09-04 21:05:51.309612', 'N', 'are_you_hungry@question.com', '홍예지', '찡찡이', 'string', 'string', '');
+
+-- study
+INSERT INTO study (study_id, create_date, modified_date, delete_yn, detail, invite_qr_code, invite_link, picture, study_name, user_id) VALUES (1, '2023-09-04 21:06:28.140570', '2023-09-04 21:06:28.140570', 'N', '화이팅', 'not yet', 'not yet', 'string', '알고리즘스터디', 1);
+
+-- notice
+INSERT INTO notice (notice_id, create_date, modified_date, contents, delete_yn, pin_yn, title, study_id, user_id) VALUES (1, '2023-09-07 00:09:20.247088', '2023-09-07 00:10:34.784027', '상세내용1', 'N', 'Y', '공지사항1', 1, 1);
+INSERT INTO notice (notice_id, create_date, modified_date, contents, delete_yn, pin_yn, title, study_id, user_id) VALUES (2, '2023-09-07 00:09:20.247088', '2023-09-07 00:10:34.784027', '상세내용2', 'N', 'Y', '공지사항2', 1, 1);
+INSERT INTO notice (notice_id, create_date, modified_date, contents, delete_yn, pin_yn, title, study_id, user_id) VALUES (3, '2023-09-07 00:09:20.247088', '2023-09-07 00:10:34.784027', '상세내용3', 'N', 'N', '공지사항3', 1, 1);
+INSERT INTO notice (notice_id, create_date, modified_date, contents, delete_yn, pin_yn, title, study_id, user_id) VALUES (4, '2023-09-07 00:09:20.247088', '2023-09-07 00:10:34.784027', '상세내용4', 'N', 'N', '공지사항4', 1, 1);
+
+-- comment
+INSERT INTO comment (comment_id, create_date, modified_date, contents, delete_yn, notice_id, parent_comment_id, user_id) VALUES (1, '2023-09-04 21:08:38.757307', '2023-09-04 21:08:38.757307', '댓글1', 'N', 1, null, 1);
+INSERT INTO comment (comment_id, create_date, modified_date, contents, delete_yn, notice_id, parent_comment_id, user_id) VALUES (3, '2023-09-04 21:08:38.757307', '2023-09-04 21:08:38.757307', '대댓글1', 'N', 1, 1, 1);
+INSERT INTO comment (comment_id, create_date, modified_date, contents, delete_yn, notice_id, parent_comment_id, user_id) VALUES (4, '2023-09-04 21:08:38.757307', '2023-09-04 21:08:38.757307', '대댓글2', 'N', 1, 1, 1);
+
+INSERT INTO comment (comment_id, create_date, modified_date, contents, delete_yn, notice_id, parent_comment_id, user_id) VALUES (2, '2023-09-04 21:08:38.757307', '2023-09-04 21:08:38.757307', '댓글2', 'N', 1, null, 1);
+
+-- round
+INSERT INTO round (round_id, create_date, modified_date, study_place, study_time, delete_yn, detail, study_id) VALUES (1, '2023-09-04 21:07:57.208247', '2023-09-06 23:02:09.487730', null, null, 'N', 'detail', 1);
+-- INSERT INTO round (round_id, create_date, modified_date, study_place, study_time, delete_yn, detail, study_id) VALUES (1, '2023-09-04 21:07:57.208247', '2023-09-06 23:02:09.487730', null, '2023-09-11 12:07:00', 'N', '오늘도 규현이와 예지누나가 숙제를 안해왔다. 뒤졌다 진짜.이제 앞으로는 안해온 사람이 밥사기로 결정하였다. 근데 벌써부터 다음주에 얻어먹을 것 같은 기분이다. 다음주부터는 시험기간', 1);
+
