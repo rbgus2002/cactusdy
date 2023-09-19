98158f67a61119513aa238ceff118b8af32b226f
rbgus2002 <rbgus2002@naver.com>
2023-09-19 20:56:53 +0900

    [B] refactor : User : repository 테스트 코드 일괄 수정 #27

--- backend/src/test/java/ssu/groupstudy/domain/user/repository/UserRepositoryTest.java
+++ backend/src/test/java/ssu/groupstudy/domain/user/repository/UserRepositoryTest.java
@@ -1,21 +1,29 @@
 package ssu.groupstudy.domain.user.repository;
 
+import org.assertj.core.api.SoftAssertions;
+import org.assertj.core.api.junit.jupiter.InjectSoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import ssu.groupstudy.domain.user.domain.User;
-import ssu.groupstudy.domain.user.dto.request.SignUpRequest;
+import ssu.groupstudy.domain.common.CustomRepositoryTest;
 
-import java.util.Optional;
-
-import static org.assertj.core.api.Assertions.*;
-
-@DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@CustomRepositoryTest
 class UserRepositoryTest {
+    @InjectSoftAssertions
+    private SoftAssertions softly;
     @Autowired
     private UserRepository userRepository;
 
+    @Test
+    @DisplayName("이메일로 사용자 존재 여부를 검사한다")
+    void existsByEmail(){
+        // given
+        final String 최규현_email = "rbgus2002@naver.com";
+
+        // when
+        boolean exist = userRepository.existsByEmail(최규현_email);
+
+        // then
+        softly.assertThat(exist).isTrue();
+    }
 }
