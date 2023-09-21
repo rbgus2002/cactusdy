2a44ec9d79a9e14eba6e64ff3b991947aaf6291c
rbgus2002 <rbgus2002@naver.com>
2023-09-21 22:29:50 +0900

    [B] feat : Task : 태스크 목록 조회 시 닉네임, 달성도, 참여태그 함께 response 하도록 구현 #30

--- backend/src/main/java/ssu/groupstudy/domain/task/dto/TaskResponse.java
+++ backend/src/main/java/ssu/groupstudy/domain/task/dto/TaskResponse.java
@@ -2,7 +2,9 @@ package ssu.groupstudy.domain.task.dto;
 
 import lombok.Getter;
 import ssu.groupstudy.domain.round.domain.RoundParticipant;
+import ssu.groupstudy.domain.round.domain.StatusTag;
 import ssu.groupstudy.domain.task.domain.Task;
+import ssu.groupstudy.domain.user.domain.User;
 
 import java.util.Comparator;
 import java.util.List;
@@ -11,12 +13,21 @@ import java.util.stream.Collectors;
 @Getter
 public class TaskResponse {
     private Long roundParticipantId;
+    private StatusTag statusTag;
     private Long userId;
+    private String nickName;
+    private Double taskProgress; // TODO : 태스크 진행율 구현 예정
     private List<TaskDto> tasks;
 
     private TaskResponse(RoundParticipant roundParticipant) {
         this.roundParticipantId = roundParticipant.getId();
-        this.userId = roundParticipant.getUser().getUserId();
+        this.statusTag = roundParticipant.getStatusTag();
+
+        User user = roundParticipant.getUser();
+        this.userId = user.getUserId();
+        this.nickName = user.getNickname();
+
+        this.taskProgress = 0.7;
         this.tasks = roundParticipant.getTasks().stream()
                 .sorted(Comparator.comparing(Task::getTaskType)
                         .thenComparing(Task::getId))
