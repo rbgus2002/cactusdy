f3115451ad8765080c25668158d685abb941a450
rbgus2002 <rbgus2002@naver.com>
2023-10-09 14:06:04 +0900

    [B] feat : User : 프로필 사진 response 추가 #28

--- backend/src/main/java/ssu/groupstudy/domain/user/dto/response/UserInfoResponse.java
+++ backend/src/main/java/ssu/groupstudy/domain/user/dto/response/UserInfoResponse.java
@@ -11,11 +11,13 @@ public class UserInfoResponse {
     private Long userId;
     private String nickname;
     private String statusMessage;
+    private String picture;
 
     public UserInfoResponse(User user){
         this.userId = user.getUserId();
         this.nickname = user.getNickname();
         this.statusMessage = user.getStatusMessage();
+        this.picture = user.getPicture();
     }
 
     public static UserInfoResponse from(User user) {
