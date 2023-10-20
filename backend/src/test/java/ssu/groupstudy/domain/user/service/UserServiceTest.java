65ef8a5e7548b214d24867e3b4f9e82706cb0138
rbgus2002 <rbgus2002@naver.com>
2023-10-20 16:54:52 +0900

    [B] test : User : 프로필 이미지 업데이트 테스트 코드 작성

--- backend/src/test/java/ssu/groupstudy/domain/user/service/UserServiceTest.java
+++ backend/src/test/java/ssu/groupstudy/domain/user/service/UserServiceTest.java
@@ -1,19 +1,29 @@
 package ssu.groupstudy.domain.user.service;
 
+import com.amazonaws.services.s3.AmazonS3;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
 import ssu.groupstudy.domain.common.ServiceTest;
 import ssu.groupstudy.domain.user.dto.request.StatusMessageRequest;
 import ssu.groupstudy.domain.user.repository.UserRepository;
 
+import java.io.IOException;
+import java.net.URL;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+
 class UserServiceTest extends ServiceTest {
     @InjectMocks
     private UserService userService;
-
     @Mock
     private UserRepository userRepository;
+    @Mock
+    private AmazonS3 amazonS3;
 
     @Test
     @DisplayName("프로필 상태메세지를 업데이트한다")
@@ -29,16 +39,18 @@ class UserServiceTest extends ServiceTest {
         softly.assertThat(최규현.getStatusMessage()).isEqualTo(newMessage);
     }
 
-//    @Test
-//    @DisplayName("프로필 이미지를 업데이트한다")
-//    void updateProfileImage(){
-//        // given
-//        String newPicture = "updated";
-//
-//        // when
-//        userService.updateProfileImage(최규현, null);
-//
-//        // then
-//        softly.assertThat(최규현.getPicture()).isEqualTo(newPicture);
-//    }
+    @Test
+    @DisplayName("프로필 이미지를 업데이트한다")
+    void updateProfileImage() throws IOException {
+        // given
+        URL newPicture = new URL("https://groupstudy-image.s3.ap-northeast-2.amazonaws.com/groupstudy-user-profile-1");
+        doReturn(newPicture).when(amazonS3).getUrl(any(), any(String.class));
+
+        // when
+        MultipartFile mock = new MockMultipartFile("tmp", new byte[1]);
+        userService.updateProfileImage(최규현, mock);
+
+        // then
+        softly.assertThat(최규현.getPicture()).isEqualTo(newPicture.toString());
+    }
 }
