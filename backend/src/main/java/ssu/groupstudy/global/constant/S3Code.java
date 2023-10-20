71a7696300f685dd19f12bfc265178910024df8f
rbgus2002 <rbgus2002@naver.com>
2023-10-20 16:59:27 +0900

    [B] refactor : S3Code : description 내용 설명 수정

--- backend/src/main/java/ssu/groupstudy/global/constant/S3Code.java
+++ backend/src/main/java/ssu/groupstudy/global/constant/S3Code.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum S3Code {
-    USER_IMAGE("groupstudy-user-profile-", "[groupstudy-image-bucket] 사용자 프로필 이미지"),
-    STUDY_IMAGE("groupstudy-study-profile-", "[groupstudy-image-bucket] 스터디 이미지"),
+    USER_IMAGE("groupstudy-user-profile-", "[사용자 프로필 이미지] prefix + userId"),
+    STUDY_IMAGE("groupstudy-study-profile-", "[스터디 이미지] prefix + studyId"),
     ;
 
     private final String prefix;
