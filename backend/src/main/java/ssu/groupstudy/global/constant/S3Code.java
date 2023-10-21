6db44e217c997e482fb5d664b8fa63c099f8e31f
rbgus2002 <rbgus2002@naver.com>
2023-10-21 17:01:29 +0900

    [B] refactor : S3Code : s3 버킷 내 프로필 이미지 저장 경로 변경

--- backend/src/main/java/ssu/groupstudy/global/constant/S3Code.java
+++ backend/src/main/java/ssu/groupstudy/global/constant/S3Code.java
@@ -6,8 +6,8 @@ import lombok.Getter;
 @AllArgsConstructor
 @Getter
 public enum S3Code {
-    USER_IMAGE("groupstudy-user-profile-", "[사용자 프로필 이미지] prefix + userId"),
-    STUDY_IMAGE("groupstudy-study-profile-", "[스터디 이미지] prefix + studyId"),
+    USER_IMAGE("profile/user/", "[사용자 프로필 이미지] prefix + userId"),
+    STUDY_IMAGE("profile/study/", "[스터디 이미지] prefix + studyId"),
     ;
 
     private final String prefix;
