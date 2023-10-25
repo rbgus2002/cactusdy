c66eff09096216368f897b6f2f502bc1de71896d
rbgus2002 <rbgus2002@naver.com>
2023-10-25 16:53:14 +0900

    [B] feat: dev 환경 분리 기초 작업

--- backend/scripts/start.sh
+++ backend/scripts/start.sh
@@ -1,21 +1,12 @@
 #!/usr/bin/env bash
 
 PROJECT_ROOT="/home/ubuntu/groupstudy"
-JAR_FILE="$PROJECT_ROOT/groupstudy-webapp.jar"
-
-APP_LOG="$PROJECT_ROOT/application.log"
-ERROR_LOG="$PROJECT_ROOT/error.log"
-DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
+JAR_FILE="$PROJECT_ROOT/groupstudy-app.jar"
 
 TIME_NOW=$(date +%c)
 
 # build 파일 복사
-echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
 cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
 # jar 파일 실행
-echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
-
-CURRENT_PID=$(pgrep -f $JAR_FILE)
-echo "$TIME_NOW > 실행된 프로세스 아이디 $CURRENT_PID 입니다." >> $DEPLOY_LOG
+nohup java --illegal-access=warn -Dcom.amazonaws.sdk.disableEc2Metadata=true -jar $JAR_FILE &
