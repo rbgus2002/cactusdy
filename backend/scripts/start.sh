d927e9a938c3a2799080bea86f1404ab7317ce57
rbgus2002 <rbgus2002@naver.com>
2023-10-25 18:14:59 +0900

    [B] bugfix: jar 실행 명령어 오류 수정

--- backend/scripts/start.sh
+++ backend/scripts/start.sh
@@ -3,10 +3,13 @@
 PROJECT_ROOT="/home/ubuntu/groupstudy"
 JAR_FILE="$PROJECT_ROOT/groupstudy-app.jar"
 
+ERROR_LOG="$PROJECT_ROOT/error.log"
+APP_LOG="$PROJECT_ROOT/app.log"
+
 TIME_NOW=$(date +%c)
 
 # build 파일 복사
 cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
 # jar 파일 실행
-nohup java --illegal-access=warn -Dcom.amazonaws.sdk.disableEc2Metadata=true -jar -Dspring.profiles.active=dev $JAR_FILE &
+nohup java --illegal-access=warn -Dcom.amazonaws.sdk.disableEc2Metadata=true -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
