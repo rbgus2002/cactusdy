e897dac6a20d13117fcf438ae964bf2ed094cdb9
arkady1356 <Arkady1356@naver.com>
2023-10-25 17:31:30 +0900

    [F] bugfix : 이미지 피커 변경에 따른 라우트 수정 누락

--- frontend/lib/routes/test_route.dart
+++ frontend/lib/routes/test_route.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:group_study_app/models/sign_info.dart';
 import 'package:group_study_app/models/task.dart';
+import 'package:group_study_app/models/user.dart';
 import 'package:group_study_app/services/auth.dart';
 import 'package:group_study_app/themes/design.dart';
 import 'package:group_study_app/widgets/image_picker_widget.dart';
 import 'package:group_study_app/widgets/panels/panel.dart';
+import 'package:image_picker/image_picker.dart';
 
 class TestRouteState extends StatefulWidget {
   const TestRouteState({
@@ -21,9 +23,13 @@ class _TestRoute extends State<TestRouteState> {
     return Scaffold(
         appBar: AppBar(title: Text("ASD")),
         body: Container(
+          child: ImagePickerWidget(onPicked: uploadProfileImage),
             padding: Design.edge15,
-            child: const ImagePickerWidget(),
         )
     );
+  }
+
+  void uploadProfileImage(XFile profileImage) async {
+    await User.updateProfileImage(profileImage);
   }
 }
--- frontend/lib/routes/work_space_route.dart
+++ frontend/lib/routes/work_space_route.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:group_study_app/models/notice_summary.dart';
+import 'package:group_study_app/models/user.dart';
 import 'package:group_study_app/themes/color_styles.dart';
 import 'package:group_study_app/themes/design.dart';
 
@@ -12,6 +13,7 @@ import 'package:group_study_app/widgets/image_picker_widget.dart';
 import 'package:group_study_app/widgets/panels/notice_panel.dart';
 import 'package:group_study_app/widgets/panels/panel.dart';
 import 'package:group_study_app/widgets/tags/study_group_tag.dart';
+import 'package:image_picker/image_picker.dart';
 
 class WorkSpaceRouteState extends StatefulWidget {
   const WorkSpaceRouteState({
@@ -31,11 +33,15 @@ class _WorkSpaceRoute extends State<WorkSpaceRouteState> {
             padding: EdgeInsets.all(20),
             child: Column(
               children: [
-                ImagePickerWidget(),
+                ImagePickerWidget(onPicked: uploadProfileImage),
               ],
             )
           )
         )
     );
+  }
+
+  void uploadProfileImage(XFile profileImage) async {
+    await User.updateProfileImage(profileImage);
   }
 }
