e0b038b4a4036bc066f06ea46527ec3a3ea9bbb5
rbgus2002 <rbgus2002@naver.com>
2023-10-26 23:13:14 +0900

    [B] refactor: createDate 순으로 규칙 가져오도록 수정

--- backend/src/main/java/ssu/groupstudy/domain/rule/repository/RuleRepository.java
+++ backend/src/main/java/ssu/groupstudy/domain/rule/repository/RuleRepository.java
@@ -4,9 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import ssu.groupstudy.domain.rule.domain.Rule;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface RuleRepository extends JpaRepository<Rule, Long> {
     @Query("SELECT r FROM Rule r WHERE r.id = :ruleId AND r.deleteYn = 'N'")
     Optional<Rule> findRuleById(Long ruleId);
+
+    @Query("SELECT r FROM Rule r WHERE r.study.studyId = :studyId AND r.deleteYn = 'N' order by r.createDate")
+    List<Rule> findRulesByStudyIdOrderByCreateDate(Long studyId);
 }
