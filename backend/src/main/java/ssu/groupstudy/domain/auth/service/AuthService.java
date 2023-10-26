51f0d2e4e76716c0a8a010e77d91ada6f52016d5
rbgus2002 <rbgus2002@naver.com>
2023-10-26 19:04:13 +0900

    [B] refactor: 문자 외부 API 호출 객체를 Utils로 감싸서 분리 (spring bean 관리)

--- backend/src/main/java/ssu/groupstudy/domain/auth/service/AuthService.java
+++ backend/src/main/java/ssu/groupstudy/domain/auth/service/AuthService.java
@@ -1,13 +1,8 @@
 package ssu.groupstudy.domain.auth.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import net.nurigo.sdk.NurigoApp;
-import net.nurigo.sdk.message.model.Message;
-import net.nurigo.sdk.message.request.SingleMessageSendingRequest;
-import net.nurigo.sdk.message.response.SingleMessageSentResponse;
-import net.nurigo.sdk.message.service.DefaultMessageService;
 import org.apache.commons.lang3.RandomStringUtils;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -23,30 +18,22 @@ import ssu.groupstudy.domain.user.dto.response.SignInResponse;
 import ssu.groupstudy.domain.user.exception.PhoneNumberExistsException;
 import ssu.groupstudy.domain.user.repository.UserRepository;
 import ssu.groupstudy.global.constant.ResultCode;
+import ssu.groupstudy.global.util.MessageUtils;
 import ssu.groupstudy.global.util.RedisUtils;
 
 @Service
 @Transactional(readOnly = true)
+@RequiredArgsConstructor
 @Slf4j
 public class AuthService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtProvider jwtProvider;
-    private final DefaultMessageService messageService;
+    private final MessageUtils messageUtils;
     private final RedisUtils redisUtils;
-    @Value("${coolsms.api.fromNum}")
-    private String fromNum;
     private final Long THREE_MINUTES = 60 * 3L;
     private final int VERIFICATION_CODE_LENGTH = 6;
 
-    public AuthService(UserRepository userRepository, PasswordEncoder passwordEncoder, JwtProvider jwtProvider, RedisUtils redisUtils,
-                       @Value("${coolsms.api.key}") String apiKey, @Value("${coolsms.api.secret.key}") String apiSecretKey, @Value("${coolsms.api.domain}") String domain) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-        this.jwtProvider = jwtProvider;
-        this.redisUtils = redisUtils;
-        this.messageService = NurigoApp.INSTANCE.initialize(apiKey, apiSecretKey, domain);
-    }
 
     @Transactional
     public SignInResponse signIn(SignInRequest request) {
@@ -79,45 +66,31 @@ public class AuthService {
     }
 
     public void sendMessageToSignUp(MessageRequest request) {
-        assertPhoneNumberDoesNotExistOrThrow(request.getPhoneNumber());
-        sendMessage(request);
+        String phoneNumber = request.getPhoneNumber();
+
+        assertPhoneNumberDoesNotExistOrThrow(phoneNumber);
+        String verificationMessage = generateVerificationMessage(phoneNumber);
+        messageUtils.sendMessage(phoneNumber, verificationMessage);
     }
 
     public void sendMessageToResetPassword(MessageRequest request){
-        assertPhoneNumberDoesExistOrThrow(request.getPhoneNumber());
-        sendMessage(request);
+        String phoneNumber = request.getPhoneNumber();
+
+        assertPhoneNumberDoesExistOrThrow(phoneNumber);
+        String verificationMessage = generateVerificationMessage(phoneNumber);
+        messageUtils.sendMessage(phoneNumber, verificationMessage);
+    }
+
+    private String generateVerificationMessage(String phoneNumber) {
+        String code = RandomStringUtils.randomNumeric(VERIFICATION_CODE_LENGTH);
+        saveCodeToRedis(code, phoneNumber);
+        return String.format("[GroupStudy] 인증번호 : %s", code); // TODO : 대괄호 안 문구 앱 이름으로 변경
     }
 
     private void assertPhoneNumberDoesExistOrThrow(String phoneNumber) {
         if (!userRepository.existsByPhoneNumber(phoneNumber)) {
             throw new PhoneNumberExistsException(ResultCode.PHONE_NUMBER_NOT_FOUND);
         }
-    }
-
-    private void sendMessage(MessageRequest request) {
-        Message message = createMessage(request);
-        SingleMessageSentResponse response = messageService.sendOne(new SingleMessageSendingRequest(message));
-        log.info("message : {}", response);
-    }
-
-    private Message createMessage(MessageRequest request) {
-        Message message = initMessage(request);
-        handleVerificationMessage(message);
-        return message;
-    }
-
-    private Message initMessage(MessageRequest request) {
-        Message message = new Message();
-        message.setFrom(fromNum);
-        message.setTo(request.getPhoneNumber());
-        return message;
-    }
-
-    private void handleVerificationMessage(Message message) {
-        String code = RandomStringUtils.randomNumeric(VERIFICATION_CODE_LENGTH);
-        saveCodeToRedis(code, message.getTo());
-        String verificationMessage = String.format("[GroupStudy] 인증번호 : %s", code); // TODO : 대괄호 안 문구 앱 이름으로 변경
-        message.setText(verificationMessage);
     }
 
     private void saveCodeToRedis(String code, String phoneNumber) {
