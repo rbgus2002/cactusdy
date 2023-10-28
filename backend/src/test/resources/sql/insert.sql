f2fbe26116518aedec2700972c0e16dc5c3bf3f3
rbgus2002 <rbgus2002@naver.com>
2023-10-28 15:47:09 +0900

    [B] test: ChecNotice 객체를 통한 equals 테스트 코드 작성

--- backend/src/test/resources/sql/insert.sql
+++ backend/src/test/resources/sql/insert.sql
@@ -17,6 +17,11 @@ INSERT INTO notice (notice_id, create_date, modified_date, contents, delete_yn, 
 INSERT INTO notice (notice_id, create_date, modified_date, contents, delete_yn, pin_yn, title, study_id, user_id) VALUES (3, '2023-09-09 00:09:20.247088', '2023-09-09 00:10:34.784027', '상세내용3', 'N', 'N', '공지사항3', 1, 1);
 INSERT INTO notice (notice_id, create_date, modified_date, contents, delete_yn, pin_yn, title, study_id, user_id) VALUES (4, '2023-09-10 00:09:20.247088', '2023-09-10 00:10:34.784027', '상세내용4', 'N', 'N', '공지사항4', 1, 1);
 
+-- check_notice
+INSERT INTO check_notice (id, notice_id, user_id) VALUES (1, 4, 1);
+INSERT INTO check_notice (id, notice_id, user_id) VALUES (2, 4, 2);
+
+
 -- comment
 INSERT INTO comment (comment_id, create_date, modified_date, contents, delete_yn, notice_id, parent_comment_id, user_id) VALUES (1, '2023-09-08 21:08:38.757307', '2023-09-08 21:08:38.757307', '댓글1', 'N', 1, null, 1);
 INSERT INTO comment (comment_id, create_date, modified_date, contents, delete_yn, notice_id, parent_comment_id, user_id) VALUES (3, '2023-09-02 21:08:38.757307', '2023-09-02 21:08:38.757307', '대댓글1', 'N', 1, 1, 1);
