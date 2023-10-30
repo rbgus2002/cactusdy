55bcf6186843282e1ff29c0c78882741ac8674a6
rbgus2002 <rbgus2002@naver.com>
2023-10-30 18:04:49 +0900

    [B] feat: ClassPath의 json으로 Firebase init하도록 구현

--- backend/src/main/java/ssu/groupstudy/global/util/FcmUtils.java
+++ backend/src/main/java/ssu/groupstudy/global/util/FcmUtils.java
@@ -6,6 +6,7 @@ import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.*;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
