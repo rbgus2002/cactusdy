6eb035bee6503f29b959da0adf7c2bda04be1edb
Arkady <Arkady1356@naver.com>
2023-11-03 02:03:24 +0900

    [F] design: 폰트 자간 추가
    자간 : 글자간 사이 간격
    letterSpacingFactor : -2%

--- frontend/lib/themes/text_styles.dart
+++ frontend/lib/themes/text_styles.dart
@@ -11,20 +11,21 @@ class TextStyles {
   static const extraBold  = FontWeight.w800;
   static const black      = FontWeight.w900;
 
-  static const TextStyle head1 = TextStyle(fontSize: 32, fontWeight: bold);
-  static const TextStyle head2 = TextStyle(fontSize: 24, fontWeight: bold);
-  static const TextStyle head3 = TextStyle(fontSize: 21, fontWeight: bold);
-  static const TextStyle head4 = TextStyle(fontSize: 18, fontWeight: semiBold);
-  static const TextStyle head5 = TextStyle(fontSize: 16, fontWeight: semiBold);
-  static const TextStyle head6 = TextStyle(fontSize: 14, fontWeight: bold);
+  //static const double letterSpacingFactor = -0.02;  // -2%
+  static const TextStyle head1 = TextStyle(fontSize: 32, letterSpacing: -0.64, fontWeight: bold);
+  static const TextStyle head2 = TextStyle(fontSize: 24, letterSpacing: -0.48, fontWeight: bold);
+  static const TextStyle head3 = TextStyle(fontSize: 21, letterSpacing: -0.42, fontWeight: bold);
+  static const TextStyle head4 = TextStyle(fontSize: 18, letterSpacing: -0.36, fontWeight: semiBold);
+  static const TextStyle head5 = TextStyle(fontSize: 16, letterSpacing: -0.32, fontWeight: semiBold);
+  static const TextStyle head6 = TextStyle(fontSize: 14, letterSpacing: -0.28, fontWeight: bold);
 
-  static const TextStyle body1 = TextStyle(fontSize: 16, fontWeight: medium);
-  static const TextStyle body2 = TextStyle(fontSize: 14, fontWeight: regular);
-  static const TextStyle body3 = TextStyle(fontSize: 13, fontWeight: regular);
-  static const TextStyle body4 = TextStyle(fontSize: 12, fontWeight: regular);
+  static const TextStyle body1 = TextStyle(fontSize: 16, letterSpacing: -0.32, fontWeight: medium);
+  static const TextStyle body2 = TextStyle(fontSize: 14, letterSpacing: -0.28, fontWeight: regular);
+  static const TextStyle body3 = TextStyle(fontSize: 13, letterSpacing: -0.26, fontWeight: regular);
+  static const TextStyle body4 = TextStyle(fontSize: 12, letterSpacing: -0.24, fontWeight: regular);
 
-  static const TextStyle caption1 = TextStyle(fontSize: 14, fontWeight: semiBold);
-  static const TextStyle caption2 = TextStyle(fontSize: 11, fontWeight: medium);
+  static const TextStyle caption1 = TextStyle(fontSize: 14, letterSpacing: -0.28, fontWeight: semiBold);
+  static const TextStyle caption2 = TextStyle(fontSize: 11, letterSpacing: -0.22, fontWeight: medium);
 
   static const TextTheme textTheme = TextTheme(
     displayLarge: head1,
