2b4c8bfcc9a1298979cccb8a19de0523926a59d8
rbgus2002 <rbgus2002@naver.com>
2023-11-02 14:00:03 +0900

    [B] test: findById 삭제 관련 테스트 코드 작성

--- backend/src/main/java/ssu/groupstudy/domain/study/repository/StudyRepository.java
+++ backend/src/main/java/ssu/groupstudy/domain/study/repository/StudyRepository.java
@@ -1,7 +1,12 @@
 package ssu.groupstudy.domain.study.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import ssu.groupstudy.domain.study.domain.Study;
 
+import java.util.Optional;
+
 public interface StudyRepository extends JpaRepository<Study, Long> {
+    @Query("SELECT s FROM Study s WHERE s.studyId = :studyId AND s.deleteYn = 'N'")
+    Optional<Study> findById(Long studyId);
 }
--- backend/src/test/java/ssu/groupstudy/domain/study/repository/StudyRepositoryTest.java
+++ backend/src/test/java/ssu/groupstudy/domain/study/repository/StudyRepositoryTest.java
@@ -2,9 +2,13 @@ package ssu.groupstudy.domain.study.repository;
 
 import org.assertj.core.api.SoftAssertions;
 import org.assertj.core.api.junit.jupiter.InjectSoftAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import ssu.groupstudy.domain.common.CustomRepositoryTest;
-import ssu.groupstudy.domain.user.repository.UserRepository;
+import ssu.groupstudy.domain.study.domain.Study;
+
+import java.util.Optional;
 
 
 @CustomRepositoryTest
@@ -13,7 +17,18 @@ class StudyRepositoryTest {
     private SoftAssertions softly;
     @Autowired
     private StudyRepository studyRepository;
-    @Autowired
-    private UserRepository userRepository;
 
+    @Test
+    @DisplayName("삭제된 스터디는 가져오지 않는다")
+    void findById(){
+        // given
+        Study study = studyRepository.findById(1L).get();
+        study.delete();
+
+        // when
+        Optional<Study> deletedStudy = studyRepository.findById(1L);
+
+        // then
+        softly.assertThat(deletedStudy).isEmpty();
+    }
 }
