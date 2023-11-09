535626d2a6e6d80a549688be963a76b69598d036
Arkady <Arkady1356@naver.com>
2023-11-09 21:26:24 +0900

    [F] bugfix: 스퀘어클 라이트, 다크 모드 전환 추가

--- frontend/lib/widgets/diagrams/squircle.dart
+++ frontend/lib/widgets/diagrams/squircle.dart
@@ -1,4 +1,6 @@
 
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 /// See Also : https://flutterappworld.com/a-flutter-study-and-comparision-of-different-squircle-shapeborder/
@@ -15,7 +17,39 @@ class SquircleBorder extends ShapeBorder {
   EdgeInsetsGeometry get dimensions => EdgeInsets.all(side.width);
 
   @override
-  ShapeBorder scale(double t) {
+  ShapeBorder? lerpFrom(ShapeBorder? a, double t) {
+    if (a is SquircleBorder) {
+      return SquircleBorder.lerp(a, this, t);
+    }
+    return super.lerpFrom(a, t);
+  }
+
+  @override
+  ShapeBorder? lerpTo(ShapeBorder? b, double t) {
+    if (b is SquircleBorder) {
+      return SquircleBorder.lerp(this, b, t);
+    }
+    return super.lerpTo(b, t);
+  }
+
+  static SquircleBorder? lerp(SquircleBorder? a, SquircleBorder? b, double t) {
+    if (identical(a, b)) {
+      return a;
+    }
+    if (a == null) {
+      return b!.scale(t);
+    }
+    if (b == null) {
+      return a.scale(1.0 - t);
+    }
+    return SquircleBorder(
+      superRadius: lerpDouble(a.superRadius, b.superRadius, t)!,
+      side: BorderSide.lerp(a.side, b.side, t),
+    );
+  }
+
+  @override
+  SquircleBorder scale(double t) {
     return SquircleBorder(
       side: side.scale(t),
       superRadius: superRadius * t,
