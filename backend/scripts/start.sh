6d375d545208c4171bd76be58e5501e240f19020
rbgus2002 <rbgus2002@naver.com>
2023-11-27 16:30:10 +0900

    [B] feat: 환경변수 추가

--- backend/scripts/start.sh
+++ backend/scripts/start.sh
@@ -10,4 +10,4 @@ TIME_NOW=$(date +%c)
 cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
 # jar 파일 실행
-nohup env GOOGLE_APPLICATION_CREDENTIALS=/home/ubuntu/groupstudy/credential/studygroup-fcm.json java --illegal-access=warn -Dcom.amazonaws.sdk.disableEc2Metadata=true -jar $JAR_FILE > $DEPLOY_LOG 2> $DEPLOY_LOG &
+nohup env GOOGLE_APPLICATION_CREDENTIALS=/home/ubuntu/groupstudy/credential/studygroup-fcm.json SENTRY_AUTH_TOKEN=sntrys_eyJpYXQiOjE3MDEwNjg2NzQuODcxMDQ1LCJ1cmwiOiJodHRwczovL3NlbnRyeS5pbyIsInJlZ2lvbl91cmwiOiJodHRwczovL3VzLnNlbnRyeS5pbyIsIm9yZyI6Imdyb3Vwc3R1ZHkifQ==_qxwQzLJv+8Apd6GSTgBb6jJs0YMhlgWdAkBZnlSnTt4LC_CTYPE=C.UTF-8 java --illegal-access=warn -Dcom.amazonaws.sdk.disableEc2Metadata=true -jar $JAR_FILE > $DEPLOY_LOG 2> $DEPLOY_LOG &
