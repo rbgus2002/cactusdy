e4e662e740e7bc93f1f5fe930d23ecee00a6a385
rbgus2002 <rbgus2002@naver.com>
2023-12-01 21:36:01 +0900

    [B] feat: 참여자 과제 콕찌르기 API 구현

--- backend/src/main/java/ssu/groupstudy/domain/notification/api/NotificationApi.java
+++ backend/src/main/java/ssu/groupstudy/domain/notification/api/NotificationApi.java
@@ -25,6 +25,16 @@ public class NotificationApi {
         return ResponseDto.success();
     }
 
+    @Operation(summary = "스터디 참여자의 과제 콕찌르기", description = "스터디 참여자가 과제를 완료하지 않았을 때 콕찌르기를 한다")
+    @GetMapping("/tasks")
+    public ResponseDto notifyTask(@AuthenticationPrincipal CustomUserDetails userDetails,
+                                  @RequestParam Long targetUserId,
+                                  @RequestParam Long studyId,
+                                  @RequestParam Long taskId) {
+        notificationService.notifyParticipantTask(userDetails.getUser(), targetUserId, studyId, taskId);
+        return ResponseDto.success();
+    }
+
     @Operation(summary = "사용자 기기 FCM 토큰 삭제", description = "사용자가 로그아웃할 때 기기의 FCM 토큰을 삭제한다")
     @DeleteMapping("/tokens")
     public ResponseDto deleteFcmToken(@AuthenticationPrincipal CustomUserDetails userDetails, @RequestParam String token) {
--- backend/src/main/java/ssu/groupstudy/domain/notification/service/NotificationService.java
+++ backend/src/main/java/ssu/groupstudy/domain/notification/service/NotificationService.java
@@ -6,6 +6,9 @@ import org.springframework.transaction.annotation.Transactional;
 import ssu.groupstudy.domain.study.domain.Study;
 import ssu.groupstudy.domain.study.exception.StudyNotFoundException;
 import ssu.groupstudy.domain.study.repository.StudyRepository;
+import ssu.groupstudy.domain.task.domain.Task;
+import ssu.groupstudy.domain.task.exception.TaskNotFoundException;
+import ssu.groupstudy.domain.task.repository.TaskRepository;
 import ssu.groupstudy.domain.user.domain.User;
 import ssu.groupstudy.domain.user.exception.UserNotFoundException;
 import ssu.groupstudy.domain.user.repository.UserRepository;
@@ -18,6 +21,7 @@ import ssu.groupstudy.global.util.FcmUtils;
 public class NotificationService {
     private final UserRepository userRepository;
     private final StudyRepository studyRepository;
+    private final TaskRepository taskRepository;
     private final FcmUtils fcmUtils;
 
     public void notifyParticipant(User me, Long targetUserId, Long studyId) {
@@ -28,6 +32,19 @@ public class NotificationService {
 
         StringBuilder body = new StringBuilder();
         body.append("'").append(me.getNickname()).append("'").append("님이 회원님을 콕 찔렀어요");
+        fcmUtils.sendNotificationByTokens(target.getFcmTokenList(), study.getStudyName(), body.toString());
+    }
+
+    public void notifyParticipantTask(User user, Long targetUserId, Long studyId, Long taskId) {
+        User target = userRepository.findById(targetUserId)
+                .orElseThrow(() -> new UserNotFoundException(ResultCode.USER_NOT_FOUND));
+        Study study = studyRepository.findById(studyId)
+                .orElseThrow(() -> new StudyNotFoundException(ResultCode.STUDY_NOT_FOUND));
+        Task task = taskRepository.findById(taskId)
+                .orElseThrow(() -> new TaskNotFoundException(ResultCode.TASK_NOT_FOUND));
+
+        StringBuilder body = new StringBuilder();
+        body.append("'").append(user.getNickname()).append("'").append("님이 회원님의 과제를 콕 찔렀어요: ").append(task.getDetail());
         fcmUtils.sendNotificationByTokens(target.getFcmTokenList(), study.getStudyName(), body.toString());
     }
 
