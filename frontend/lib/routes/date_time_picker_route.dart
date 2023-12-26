e415508589e648b91bf07bb6a57328a01d9b6f42
Arkady <Arkady1356@naver.com>
2023-12-26 19:07:36 +0900

    [F] feat: 날짜를 선택 해제 할 수 있도록 개선

--- frontend/lib/routes/date_time_picker_route.dart
+++ frontend/lib/routes/date_time_picker_route.dart
@@ -28,7 +28,8 @@ class DateTimePickerRoute extends StatefulWidget {
 
 
 class _DateTimePickerRouteState extends State<DateTimePickerRoute> {
-  late DateTime _date;
+  late final DateTime _initDate;
+  late DateTime? _date;
   late DateTime _time;
 
   @override
@@ -36,7 +37,8 @@ class _DateTimePickerRouteState extends State<DateTimePickerRoute> {
     super.initState();
 
     _date = widget.round.studyTime??DateTime.now();
-    _time = widget.round.studyTime??DateTime.now();
+    _time = widget.round.studyTime??DateTime(0, 0, 0, 12, 0);
+    _initDate = _date!;
   }
 
   @override
@@ -54,9 +56,9 @@ class _DateTimePickerRouteState extends State<DateTimePickerRoute> {
               padding: const EdgeInsets.symmetric(horizontal: 20),
               child: CustomCalendarDatePicker(
                 onDateChanged: _pickDate,
-                initialDate: _date,
-                firstDate: DateTime(2000),
-                lastDate: DateTime(2100),),),
+                initialDate: _initDate,
+                firstDate: DateTime(2000, 1, 1),
+                lastDate: DateTime(2100, 12, 31),),),
 
             // Time Picker Button
             Container(
@@ -65,10 +67,12 @@ class _DateTimePickerRouteState extends State<DateTimePickerRoute> {
                   top: BorderSide(
                       color: context.extraColors.grey200!),),),
               child: InkWell(
-                onTap: () => BottomSheets.timePickerBottomSheet(
-                  context: context,
-                  initTime: _time,
-                  onSelected: _pickTime,),
+                onTap: (_date != null)?
+                    () => BottomSheets.timePickerBottomSheet(
+                      context: context,
+                      initTime: _time,
+                      onSelected: _pickTime,) :
+                    null,
                 child: Padding(
                   padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
                   child: Row(
@@ -79,7 +83,9 @@ class _DateTimePickerRouteState extends State<DateTimePickerRoute> {
                           style: TextStyles.head4,),),
 
                       Text(
-                        TimeUtility.getTime(_time),
+                        (_date != null) ?
+                          TimeUtility.getTime(_time) :
+                          '- -  - - : - -',
                         style: TextStyles.head4.copyWith(color: ColorStyles.mainColor),),
                       Design.padding8,
 
@@ -108,7 +114,7 @@ class _DateTimePickerRouteState extends State<DateTimePickerRoute> {
     );
   }
 
-  void _pickDate(DateTime date) {
+  void _pickDate(DateTime? date) {
     setState(() => _date = date);
   }
 
@@ -117,7 +123,15 @@ class _DateTimePickerRouteState extends State<DateTimePickerRoute> {
   }
 
   void _updateStudyTime() async {
-    DateTime newStudyTime = DateTime(_date.year, _date.month, _date.day, _time.hour, _time.minute);
+    DateTime? newStudyTime;
+    if (_date != null) {
+      newStudyTime = DateTime(
+          _date!.year,
+          _date!.month,
+          _date!.day,
+          _time.hour,
+          _time.minute);
+    }
 
     try {
       widget.round.studyTime = newStudyTime;
