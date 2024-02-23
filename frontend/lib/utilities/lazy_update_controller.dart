2eb8e91dd3a88460bfe9edea774f34db5546b948
Arkady <Arkady1356@naver.com>
2024-02-23 20:33:52 +0900

    [F] feat: lazy update 추가
    변경 사항을 즉시 반영하지 않고
    n초 후 처음 상태와 비교후 변경시에만 반영

---
+++ frontend/lib/utilities/lazy_update_controller.dart
@@ -0,0 +1,57 @@
+
+
+import 'dart:async';
+import 'dart:ui';
+
+import 'package:groupstudy/services/logger.dart';
+
+class LazyUpdateController<T> {
+  static Logger logger = Logger('lazyUpdateController');
+
+  final VoidCallback onUpdate;
+  late T _lastState;
+  late T _currentState;
+
+  Timer? _timer;
+  int _restTime = 0;
+
+  LazyUpdateController({
+    required T initState,
+    required this.onUpdate,
+  }) {
+    _lastState = initState;
+    _currentState = initState;
+  }
+
+  void lazyUpdate(T newState, int waitingTime) {
+    _restTime = waitingTime;
+    _currentState = newState;
+
+    _timer ??= Timer.periodic(const Duration(seconds: 1), (timer) {
+      // stop timer
+      if (_restTime <= 0) {
+        update();
+      }
+      else {
+        --_restTime;
+      }
+    });
+  }
+
+  void update() {
+    if (_lastState != _currentState) {
+      _lastState = _currentState;
+      onUpdate();
+    }
+
+    _reset();
+  }
+
+  /// reset timer and stab count
+  void _reset() {
+    if (_timer != null) {
+      _timer!.cancel();
+      _timer = null;
+    }
+  }
+}
