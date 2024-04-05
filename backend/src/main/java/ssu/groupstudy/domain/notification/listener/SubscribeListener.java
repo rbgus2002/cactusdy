793f78ef01aacb3c9703e18e39b0ae651c484057
rbgus2002 <rbgus2002@naver.com>
2024-04-05 15:54:34 +0900

    [B] refactor: 로그 호출 위치 변경

--- backend/src/main/java/ssu/groupstudy/domain/notification/listener/SubscribeListener.java
+++ backend/src/main/java/ssu/groupstudy/domain/notification/listener/SubscribeListener.java
@@ -24,39 +24,39 @@ public class SubscribeListener {
 
     @EventListener
     public void handleAllUserTopicSubscribeEvent(AllUserTopicSubscribeEvent event) {
+        log.info("## handleAllUserTopicSubscribeEvent : ");
         User user = event.getUser();
         fcmUtils.subscribeTopicFor(user.getFcmTokenList(), TopicCode.ALL_USERS, null);
-        log.info("## handleAllUserTopicSubscribeEvent : ");
     }
 
     @EventListener
     public void handleNoticeTopicSubscribeEvent(NoticeTopicSubscribeEvent event) {
+        log.info("## handleNoticeTopicSubscribeEvent : ");
         User user = event.getUser();
         fcmUtils.subscribeTopicFor(user.getFcmTokenList(), TopicCode.NOTICE, event.getNoticeId());
-        log.info("## handleNoticeTopicSubscribeEvent : ");
     }
 
     @EventListener
     public void handleStudyTopicSubscribeEvent(StudyTopicSubscribeEvent event) {
+        log.info("## handleStudyTopicSubscribeEvent : ");
         User user = event.getUser();
         fcmUtils.subscribeTopicFor(user.getFcmTokenList(), TopicCode.STUDY, event.getStudyId());
-        log.info("## handleStudyTopicSubscribeEvent : ");
     }
 
     @EventListener
     public void handleStudyTopicUnSubscribeEvent(StudyTopicUnsubscribeEvent event) {
+        log.info("## handleStudyTopicUnSubscribeEvent : ");
         User user = event.getUser();
         fcmUtils.unsubscribeTopicFor(user.getFcmTokenList(), TopicCode.STUDY, event.getStudyId());
-        log.info("## handleStudyTopicUnSubscribeEvent : ");
     }
 
     @EventListener
     public void handleNoticeTopicUnSubscribeEvent(NoticeTopicUnsubscribeEvent event) {
+        log.info("## handleNoticeTopicUnSubscribeEvent : ");
         User user = event.getUser();
         for (Notice notice : event.getNotices()) {
             fcmUtils.unsubscribeTopicFor(user.getFcmTokenList(), TopicCode.NOTICE, notice.getNoticeId());
         }
-        log.info("## handleNoticeTopicUnSubscribeEvent : ");
     }
 
 }
