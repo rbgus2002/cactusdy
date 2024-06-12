62d3c74e650cb8cde0b3ccbd707a1f1887e468db
Arkady <Arkady1356@naver.com>
2024-06-13 01:47:35 +0900

    [FE] # 서버 base URL 수정

--- frontend/lib/services/database_service.dart
+++ frontend/lib/services/database_service.dart
@@ -6,8 +6,8 @@ class DatabaseService {
   DatabaseService._();
 
   static late final String serverUrl;
-  static const String _serverUrlProd = 'http://3.39.16.245:8080/';
-  static const String _serverUrlDev = 'http://43.200.247.214:8080/';
+  static const String _serverUrlProd = 'http://54.180.56.102:8080/';
+  static const String _serverUrlDev = 'http://54.180.56.102:8080/';
 
   /// For Flavor
   static void init(FlavorType flavor) {
