26728d7b1a670fc222ffed8c0601500140bc792b
rbgus2002 <rbgus2002@naver.com>
2024-06-13 22:52:08 +0900

    [BE] #223 TODO 주석 추가

--- backend/src/main/java/ssu/groupstudy/global/config/OpenEntityManagerConfig.java
+++ backend/src/main/java/ssu/groupstudy/global/config/OpenEntityManagerConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter;
 
 @Configuration
-public class OpenEntityManagerConfig {
+public class OpenEntityManagerConfig { // [2024-06-13:최규현] TODO: param 변경 후 삭제 고려
     @Bean
     public FilterRegistrationBean<OpenEntityManagerInViewFilter> openEntityManagerInViewFilter() {
         FilterRegistrationBean<OpenEntityManagerInViewFilter> bean = new FilterRegistrationBean<>();
