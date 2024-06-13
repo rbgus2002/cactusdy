bc66543ad188b1593aa9d9c7cc84789b7a8ef203
rbgus2002 <rbgus2002@naver.com>
2024-06-13 23:10:32 +0900

    [BE] #223 레디스 설정 객체 이름 변경

--- backend/src/main/java/ssu/groupstudy/global/config/RedisConfig.java
+++ backend/src/main/java/ssu/groupstudy/global/config/RedisConfig.java
@@ -8,7 +8,7 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 @Configuration
-public class RedisAuthConfig {
+public class RedisConfig {
     @Value("${spring.redis.host}")
     private String host;
     @Value("${spring.redis.port}")
