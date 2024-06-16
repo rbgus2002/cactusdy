26e0b5e6ccfdc8cb14a85267cf00b02191fdbda6
rbgus2002 <rbgus2002@naver.com>
2024-06-16 22:21:45 +0900

    [BE] # spring 기준 시간 KTC 변경

--- backend/src/main/java/ssu/groupstudy/GroupstudyApplication.java
+++ backend/src/main/java/ssu/groupstudy/GroupstudyApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -13,5 +16,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class GroupstudyApplication {
     public static void main(String[] args) {
         SpringApplication.run(GroupstudyApplication.class, args);
+    }
+
+    @PostMapping
+    public void setTimeZone() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 }
