b17c5e88e8cb54cc3dcfa0b22f2a1bad6d4ad5ba
rbgus2002 <rbgus2002@naver.com>
2024-07-03 22:08:21 +0900

    [BE] # spring 기준 시간 KTC 변경 오류 수정

--- backend/src/main/java/ssu/groupstudy/GroupstudyApplication.java
+++ backend/src/main/java/ssu/groupstudy/GroupstudyApplication.java
@@ -5,9 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -16,10 +13,5 @@ import java.util.TimeZone;
 public class GroupstudyApplication {
     public static void main(String[] args) {
         SpringApplication.run(GroupstudyApplication.class, args);
-    }
-
-    @PostMapping
-    public void setTimeZone() {
-        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
     }
 }
--- backend/src/main/java/ssu/groupstudy/domain/common/entity/BaseEntity.java
+++ backend/src/main/java/ssu/groupstudy/domain/common/entity/BaseEntity.java
@@ -6,12 +6,12 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Column;
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @Getter
+
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
@@ -22,4 +22,15 @@ public class BaseEntity {
     @LastModifiedDate
     @Column(nullable = false)
     private LocalDateTime modifiedDate;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createDate = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
+        this.modifiedDate = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.modifiedDate = LocalDateTime.now(ZoneId.of("Asia/Seoul"));
+    }
 }
