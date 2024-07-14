b5bfac98009588250c2c19f6c3385bffededd9be
rbgus2002 <rbgus2002@naver.com>
2024-07-14 17:10:02 +0900

    [BE] #233 findById 메소드 추가에 따른 TC 수정

--- backend/src/test/java/ssu/groupstudy/domain/user/service/UserServiceTest.java
+++ backend/src/test/java/ssu/groupstudy/domain/user/service/UserServiceTest.java
@@ -9,6 +9,11 @@ import ssu.groupstudy.domain.common.ServiceTest;
 import ssu.groupstudy.domain.user.repository.UserEntityRepository;
 import ssu.groupstudy.global.util.S3Utils;
 
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+
 class UserServiceTest extends ServiceTest {
     @InjectMocks
     private UserService userService;
@@ -23,6 +28,8 @@ class UserServiceTest extends ServiceTest {
         @DisplayName("회원 탈퇴 시 사용자의 닉네임, 상태메세지, 프로필 사진을 모두 삭제한다")
         void deleteUserInfo(){
             // given
+            doReturn(Optional.of(최규현)).when(userEntityRepository).findById(any(Long.class));
+
             // when
             userService.removeUser(최규현.getUserId());
 
@@ -37,6 +44,7 @@ class UserServiceTest extends ServiceTest {
         @DisplayName("회원 탈퇴 시 가지는 FCM 토큰을 모두 삭제한다")
         void deleteAllFcmTokens(){
             // given
+            doReturn(Optional.of(최규현)).when(userEntityRepository).findById(any(Long.class));
             최규현.addFcmToken("token1");
             최규현.addFcmToken("token2");
             최규현.addFcmToken("token3");
