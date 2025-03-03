ba1bfbccd2f884b1f4c24a194be1f9bc038461f5
rbgus2002 <rbgus2002@naver.com>
2025-03-03 16:00:47 +0900

    #257 Getter 추가

--- backend/src/main/java/ssu/groupstudy/api/common/vo/IdResVo.java
+++ backend/src/main/java/ssu/groupstudy/api/common/vo/IdResVo.java
@@ -2,7 +2,9 @@ package ssu.groupstudy.api.common.vo;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class IdResVo {
     private Long id;
