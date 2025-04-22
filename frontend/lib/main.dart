42208687c14420574c9c404f6676a4c463a07a02
Arkady <Arkady1356@naver.com>
2025-04-23 00:06:27 +0900

    [FE] #267 Replace `uni_link` to `app_link` for Deep Link
    - `uni_link` is deprecated.

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -4,11 +4,11 @@ import 'package:flutter/material.dart';
 import 'package:groupstudy/l10n/app_localizations.dart';
 import 'package:groupstudy/models/sign_info.dart';
 import 'package:groupstudy/routes/splash_route.dart';
+import 'package:groupstudy/services/app_link_service.dart';
 import 'package:groupstudy/services/database_service.dart';
 import 'package:groupstudy/services/flavor.dart';
 import 'package:groupstudy/services/kakao_service.dart';
 import 'package:groupstudy/services/message_service.dart';
-import 'package:groupstudy/services/uri_link_service.dart';
 import 'package:groupstudy/themes/app_theme.dart';
 import 'package:groupstudy/utilities/extensions.dart';
 
@@ -18,7 +18,7 @@ void main() async {
   LocalizationExtension.init();
   MessageService.init();
   KakaoService.init();
-  UriLinkService.init();
+  AppLinkService.init();
   AppTheme.init();
 
   await Flavor.init((flavor) {
--- frontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,7 @@
 import FlutterMacOS
 import Foundation
 
+import app_links
 import file_selector_macos
 import firebase_analytics
 import firebase_core
@@ -17,6 +18,7 @@ import shared_preferences_foundation
 import sqflite_darwin
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
