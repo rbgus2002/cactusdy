af7e9af992f2460d9c3ef4f10edb7758a17f06cd
rbgus2002 <rbgus2002@naver.com>
2025-05-04 22:40:38 +0900

    #265 댓글 생성 API 비동기 처리

--- backend/src/main/java/ssu/groupstudy/global/util/FcmUtils.java
+++ backend/src/main/java/ssu/groupstudy/global/util/FcmUtils.java
@@ -5,6 +5,7 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.*;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import ssu.groupstudy.domain.common.enums.TopicCode;
 
@@ -49,6 +50,7 @@ public class FcmUtils {
         FirebaseMessaging.getInstance().sendEachForMulticastAsync(message);
     }
 
+    @Async
     public void sendNotificationToTopic(String title, String body, TopicCode topicCode, Long id, Map<String, String> data) {
         log.info("## sendNotificationToTopic : title = {}, body = {}", title, body);
         String topic = topicCode.formatTopic(id);
