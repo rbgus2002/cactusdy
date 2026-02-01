03f3c16b6f238988d4a65a5326e0b2d38fa59b3a
rbgus2002 <rbgus2002@naver.com>
2026-02-01 20:41:03 +0900

    #298 NotificationService 신규 적용 -> CommentService

--- backend/src/main/java/ssu/groupstudy/domain/notification/service/NotificationService.java
+++ backend/src/main/java/ssu/groupstudy/domain/notification/service/NotificationService.java
@@ -1,32 +1,63 @@
 package ssu.groupstudy.domain.notification.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
 import ssu.groupstudy.domain.common.enums.TopicCode;
 import ssu.groupstudy.domain.notification.param.NotificationParam;
+import ssu.groupstudy.domain.study.entity.ParticipantEntity;
+import ssu.groupstudy.domain.study.repository.ParticipantEntityRepository;
 import ssu.groupstudy.global.util.FcmUtils;
 
 import javax.annotation.Nullable;
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 @Slf4j
-public abstract class NotificationService {
-    @Autowired
-    protected FcmUtils fcmUtils;
+public class NotificationService {
+    private final FcmUtils fcmUtils;
+    private final NotificationHistoryService notificationHistoryService;
+    private final ParticipantEntityRepository participantEntityRepository;
 
-    public final void push(NotificationParam param) {
-        pushToFcm(param);
-        saveNotificationHistory();
+    public void push(NotificationParam param) {
+        fcmUtils.sendNotificationToTopic(
+                param.getTitle(),
+                param.getBody(),
+                param.getTopicCode(),
+                param.getTopicId(),
+                param.getData()
+        );
+        saveNotificationHistory(param);
     }
 
-    protected abstract void pushToFcm(NotificationParam param);
-
-    public void subscribeToFcm(List<String> fcmTokens, TopicCode topicCode, @Nullable Long id){
+    @Async
+    public void subscribeToFcm(List<String> fcmTokens, TopicCode topicCode, @Nullable Long id) {
         fcmUtils.subscribeTopicFor(fcmTokens, topicCode, id);
     }
 
-    private void saveNotificationHistory() {
-        // TODO() : save notification history
-        log.debug("SAVE!!!!!!!!!!!!! TMP!!!!!!!!!!!!!");
+    @Async
+    public void unsubscribeToFcm(List<String> fcmTokens, TopicCode topicCode, @Nullable Long id) {
+        fcmUtils.unsubscribeTopicFor(fcmTokens, topicCode, id);
+    }
+
+    private void saveNotificationHistory(NotificationParam param) {
+        Long studyId = param.getStudyId();
+        if (studyId == null) {
+            log.debug("Skip saveNotificationHistory: studyId is null");
+            return;
+        }
+
+        List<ParticipantEntity> participants = participantEntityRepository.findAllActiveByStudyId(studyId);
+        participants.forEach(participant ->
+                notificationHistoryService.saveNotificationHistory(
+                        participant.getUser(),
+                        param.getNotificationDataType(),
+                        param.getTitle(),
+                        param.getBody(),
+                        param.getData()
+                )
+        );
     }
 }
